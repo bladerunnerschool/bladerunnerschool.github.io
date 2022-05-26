c3919b4a20914209f716a8d436e3ae02747f3d4a
bladerunnerschool <105100540+bladerunnerschool@users.noreply.github.com>
2022-05-26 11:48:50 +0200

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -350,6 +350,8 @@
 if ( document.URL.includes('index.html') ) {
   // sistemo la sorgente dell'iframe ogni volta che chiudo uno finestra, anche se non quella giusta
   document.getElementById('itaIframe').src='https://www.youtube.com/embed/1BY0XjWtc-c';
+	window.alert("https://www.youtube.com/embed/1BY0XjWtc-c");
+
 } 
 // pagina in inglese = Trad.html
 else if ( document.URL.includes('Trad.html') ) {
